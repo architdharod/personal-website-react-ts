90eafb0338a82f6b44b4a24c5973d2dbec71f946
Archit Dharod <archit.dharod@pflanzmich.de>
2023-02-17 06:43:13 +0100

    added more css, including social media icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,18 @@ export default function Footer(){
         <footer>
             <button className="cv-button">Get a free copy of my CV!</button>
             <div className="social-media">
-                <a>GitHub</a>
-                <a>Docker Hub</a>
-                <a>LinkedIn</a>
-                <a>Instagram</a>
+                <a href="https://github.com/architdharod" target="#">
+                    <i className="bi bi-github"></i>
+                </a>
+                <a href="https://www.instagram.com/architrakatha" target="#">
+                    <i className="bi bi-instagram"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/archit-dharod/" target="#">
+                    <i className="bi bi-linkedin"></i>
+                </a>
             </div>
             <h3>Designed and made by me (with a little help from my friends)</h3>
-            <h4>Heavily inspired from <a href="https://gumroad.com">gumroad.com</a></h4>
+            <h4>Heavily inspired from <a href="https://gumroad.com" target="#">gumroad.com</a></h4>
         </footer>
     )
 }
